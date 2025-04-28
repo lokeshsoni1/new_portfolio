0853401dcf8d53bfd8ba06e36c1cb436dbd7e645
lokeshsoni1 <verrmaji001@gmail.com>
2025-04-28 22:52:36 +0530

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-// Simple form submission handling
-const form = document.getElementById('contact-form');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for contacting me! I will get back to you soon.');
-    form.reset();
-});
